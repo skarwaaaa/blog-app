9898491cfd502ead468b74f5f723e8fe4c8de086
skarwaaaa <skarwaaaa@gmail.com>
2021-05-31 01:58:22 +0900

    タイトル一覧表示

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
   # return HttpResponse("Hello World!")
-  return render(request, 'posts/index.html')
+  posts = Post.objects.order_by('-published')
+  return render(request, 'posts/index.html', {'posts': posts})
