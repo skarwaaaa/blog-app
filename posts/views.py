141247264de2ba65aa32e0ce9f26dddc0c205b18
skarwaaaa <skarwaaaa@gmail.com>
2021-06-08 23:14:46 +0900

    page not findの設定

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -8,5 +8,5 @@ def index(request):
   return render(request, 'posts/index.html', {'posts': posts})
 
 def post_detail(request, post_id):
-  post = Post.objects.get(pk=post_id)#pk= indexの番号のこと
+  post = get_object_or_404(Post, pk=post_id)#pk= indexの番号のこと
   return render(request, 'posts/post_detail.html', {'post':post})
